49767852d8f6c3ffa55c75a8ed6f3b336477f580
Filin <filin@ymail.com>
2022-09-22 23:26:15 +0300

    Add nginx csv log

--- nginx_parser.py
+++ nginx_parser.py
@@ -17,7 +17,6 @@ except:
 pattern = re.compile(r'(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<datetime>\d{2}\/[A-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\] ((?P<getpost>\"(GET|POST) )(?P<url>.+)\") (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>.+)["]) (["](?P<useragent>.+)["]) (?P<num1>\d+) (?P<num2>\d+.\d+) \[(?P<user>.+)\] (?P<brack>\[\]) (?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}) (?P<num3>\d+) (?P<num4>\d+.\d+) (?P<code>\d+) (?P<id>.+)')
 
 csv_header = ['host', 'datetime', 'getpost', 'url', 'statuscode', 'bytessent', 'refferer', 'useragent', 'num1', 'num2', 'user', 'brack', 'ip', 'num3', 'num4', 'code', 'id']
-csv_data = []
 
 file = open(log_file_name)
 
